83732c5f92fd4e2c204cc69d370b75c43992a08c
= <=>
2026-04-23 22:25:14 -0500

    feat: init people ms project

---
+++ applications/app-service/src/main/java/co/com/pragma/peoplems/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.com.pragma.peoplems.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.com.pragma.peoplems.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
