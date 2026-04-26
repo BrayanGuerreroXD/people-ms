dedb9650239b66214d179388ae0c965f25801fa7
Brayan Guerrero <brayanstewartguor@ufps.edu.co>
2026-04-26 08:49:14 -0500

    feat(usecase): implement get person use case

--- domain/usecase/src/main/java/co/com/pragma/peoplems/usecase/getperson/GetPersonUseCase.java
+++ domain/usecase/src/main/java/co/com/pragma/peoplems/usecase/getperson/GetPersonUseCase.java
@@ -1,25 +1,32 @@
 package co.com.pragma.peoplems.usecase.getperson;
 
+import co.com.pragma.peoplems.model.exception.GlobalExceptionEnum;
+import co.com.pragma.peoplems.model.exception.NotFoundException;
 import co.com.pragma.peoplems.model.person.Person;
+import co.com.pragma.peoplems.model.person.gateways.PersonRepository;
 import lombok.RequiredArgsConstructor;
-
-import java.util.List;
-import java.util.Optional;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
 public class GetPersonUseCase implements GetPersonService {
+
+    private final PersonRepository personRepository;
+
     @Override
-    public Optional<Person> getById(Long id) {
-        return Optional.empty();
+    public Mono<Person> getById(Long id) {
+        return personRepository.findById(id)
+                .switchIfEmpty(Mono.error(
+                        new NotFoundException(GlobalExceptionEnum.PERSON_NOT_FOUND)));
     }
 
     @Override
-    public Optional<Person> getByEmail(String email) {
-        return Optional.empty();
+    public Mono<Person> getByEmail(String email) {
+        return personRepository.findByEmail(email);
     }
 
     @Override
-    public List<Person> getAll() {
-        return List.of();
+    public Flux<Person> getAll() {
+        return personRepository.findAllNonAdmin();
     }
 }
