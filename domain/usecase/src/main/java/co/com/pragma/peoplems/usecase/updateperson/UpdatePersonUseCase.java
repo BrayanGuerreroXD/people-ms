6b54532e127531eae1cf15e04672c345b8a8bb86
Brayan Guerrero <brayanstewartguor@ufps.edu.co>
2026-04-26 08:49:15 -0500

    feat(usecase): implement update person use case

--- domain/usecase/src/main/java/co/com/pragma/peoplems/usecase/updateperson/UpdatePersonUseCase.java
+++ domain/usecase/src/main/java/co/com/pragma/peoplems/usecase/updateperson/UpdatePersonUseCase.java
@@ -1,14 +1,33 @@
 package co.com.pragma.peoplems.usecase.updateperson;
 
+import co.com.pragma.peoplems.model.exception.GlobalExceptionEnum;
+import co.com.pragma.peoplems.model.exception.NotFoundException;
 import co.com.pragma.peoplems.model.person.Person;
+import co.com.pragma.peoplems.model.person.gateways.PersonRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 public class UpdatePersonUseCase implements UpdatePersonService {
 
-    @Override
-    public Person update(Person person) {
-        return null;
-    }
+    private final PersonRepository personRepository;
 
+    @Override
+    @Transactional
+    public Mono<Person> update(Long id, Person personData) {
+        return personRepository.findById(id)
+                .switchIfEmpty(Mono.error(
+                        new NotFoundException(GlobalExceptionEnum.PERSON_NOT_FOUND)))
+                .flatMap(existing -> {
+                    Person updated = existing.toBuilder()
+                            .name(personData.getName())
+                            .age(personData.getAge())
+                            .updatedAt(LocalDateTime.now())
+                            .build();
+                    return personRepository.save(updated);
+                });
+    }
 }
