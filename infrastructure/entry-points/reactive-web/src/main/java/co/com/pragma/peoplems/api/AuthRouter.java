3d3e4d5731b52a96fc9209d3b07e5e4cbc0a6181
Brayan Guerrero <brayanstewartguor@ufps.edu.co>
2026-04-27 09:05:54 -0500

    feat(api): add @RouterOperations to AuthRouter for swagger discovery

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/peoplems/api/AuthRouter.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/peoplems/api/AuthRouter.java
@@ -1,8 +1,12 @@
 package co.com.pragma.peoplems.api;
 
 import co.com.pragma.peoplems.api.handler.AuthHandler;
+import org.springdoc.core.annotations.RouterOperation;
+import org.springdoc.core.annotations.RouterOperations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
@@ -12,6 +16,14 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 @Configuration
 public class AuthRouter {
 
+    @RouterOperations({
+            @RouterOperation(path = "/api/auth/login", method = RequestMethod.POST,
+                    beanClass = AuthHandler.class, beanMethod = "login",
+                    produces = MediaType.APPLICATION_JSON_VALUE),
+            @RouterOperation(path = "/api/auth/logout", method = RequestMethod.POST,
+                    beanClass = AuthHandler.class, beanMethod = "logout",
+                    produces = MediaType.APPLICATION_JSON_VALUE)
+    })
     @Bean
     public RouterFunction<ServerResponse> authRoutes(AuthHandler authHandler) {
         return route(POST("/api/auth/login"), authHandler::login)
